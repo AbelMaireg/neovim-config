4526dcd3048b3fd6a2d0ed410208b0861533bf6e
AbelMaireg <abelrighthere@gmail.com>
2025-01-27 01:48:39 +0300

    feat: options set with out clipboard configuration

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,26 @@
 require "nvchad.options"
 
--- add yours here!
+local api = vim.api
+local opt = vim.opt
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- visuals
+opt.cursorline = true
+opt.cursorlineopt = "both"
+api.nvim_set_hl(0, "CursorLine", { bg = "#202020" })
+api.nvim_set_hl(0, "Visual", { bg = "#505050", underline = true })
+opt.scrolloff = 10
+opt.relativenumber = true
+
+opt.foldmethod = "indent"
+opt.foldenable = true
+opt.foldlevel = 99
+opt.foldnestmax = 10
+opt.foldlevelstart = 99
+
+opt.clipboard = "unnamedplus"
+
+opt.shiftwidth = 4
+
+opt.swapfile = false
+opt.backup = false
+opt.writebackup = false
