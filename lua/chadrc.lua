af5205cecf70eae934a7ad7431901caa26b81cb2
AbelMaireg <abelrighthere@gmail.com>
2025-01-27 01:59:42 +0300

    chadrc set

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,24 +1,59 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
-M.base46 = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+M.ui = {
+    statusline = {
+        theme = "minimal",
+        separator_style = "block",
+    },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+M.base46 = {
+    theme = "onedark",
+    transparency = true,
+
+    hl_override = {
+        ["@comment"] = { fg = "#afafaf", italic = true },
+    },
+}
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        if vim.fn.argc() == 1 and vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
+            require("nvim-tree.api").tree.open()
+        end
+    end,
+})
+
+require("nvterm").setup {
+    terminals = {
+        shell = vim.o.shell,
+        list = {},
+        type_opts = {
+            float = {
+                relative = "editor",
+                row = 0.1,
+                col = 0.1,
+                width = 0.8,
+                height = 0.8,
+                border = "single",
+            },
+            horizontal = { location = "rightbelow", split_ratio = 0.33 },
+            vertical = { location = "rightbelow", split_ratio = 0.33 },
+        },
+    },
+    behavior = {
+        autoclose_on_quit = {
+            enabled = false,
+            confirm = true,
+        },
+        close_on_exit = true,
+        auto_insert = true,
+    },
+}
 
 return M
