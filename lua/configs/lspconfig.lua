73cfe7ae080c958539db22fa02ee14dc7dc8d5f2
AbelMaireg <abelrighthere@gmail.com>
2025-02-06 15:13:57 +0300

    feat: vim.lsp abilites added volar setup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,46 +4,11 @@ local lspconfig = require("lspconfig")
 local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 
-mason.setup({})
-mason_lspconfig.setup({
-    ensure_installed = {
-        "ts_ls",
-        "volar",
-    },
-    handler = {
-        function(server)
-            lspconfig[server].setup({})
-        end,
-        volar = function()
-            lspconfig.volar.setup({})
-        end,
-        ts_ls = function()
-            local vue_typescript_plugin_path = vim.fn.stdpath("data")
-                ..
-                "/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin"
-
-            lspconfig.ts_ls.setup({
-                init_options = {
-                    plugins = {
-                        {
-                            name = "@vue/typescript-plugin",
-                            location = vue_typescript_plugin_path,
-                            languages = { "vue" },
-                        },
-                    },
-                },
-                filetypes = {
-                    "typescript",
-                    "javascript",
-                    "javascriptreact",
-                    "typescriptreact",
-                    "vue",
-                },
-                single_file_support = false,
-            })
-        end,
-    },
-})
+local vim_lsp_api_bindings = {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+}
 
 local servers = {
     "bashls",
@@ -62,13 +27,51 @@ local servers = {
     "yamlls",
 }
 
-for _, lsp in ipairs(servers) do
-    local config = {
+function ts_ls_setup()
+    local vue_typescript_plugin_path = vim.fn.stdpath("data")
+        .. "/mason/packages/vue-language-server/node_modules"
+        .. "/@vue/language-server/node_modules"
+        .. "/@vue/typescript-plugin"
+
+    lspconfig.ts_ls.setup({
         on_attach = on_attach,
         on_init = on_init,
         capabilities = capabilities,
-    }
+        single_file_support = false,
+        init_options = {
+            plugins = {
+                {
+                    name = "@vue/typescript-plugin",
+                    location = vue_typescript_plugin_path,
+                    languages = { "vue" },
+                },
+            },
+        },
+        filetypes = {
+            "typescript",
+            "javascript",
+            "javascriptreact",
+            "typescriptreact",
+            "vue",
+        },
+    })
+end
 
+mason.setup({})
+mason_lspconfig.setup({
+    ensure_installed = servers,
+    handler = {
+        function(server)
+            lspconfig[server].setup({ vim_lsp_api_bindings })
+        end,
+        volar = function()
+            lspconfig.volar.setup({ vim_lsp_api_bindings })
+        end,
+        ts_ls = ts_ls_setup(),
+    },
+})
+
+for _, lsp in ipairs(servers) do
     -- if lsp == "denols" then
     --     config.root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
     -- elseif lsp == "ts_ls" then
