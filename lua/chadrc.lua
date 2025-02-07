ed8bd5c9cc297888d0c55f136cf2f2e1aa4d72c2
AbelMaireg <abelrighthere@gmail.com>
2025-02-07 22:15:41 +0300

    wip: nvchad.ui status line configuration unable to be set in chadrc instead it is set from the plugin module directly.

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,8 +5,44 @@ local M = {}
 
 M.ui = {
     statusline = {
-        theme = "minimal",
-        separator_style = "block",
+        theme = "vscode_colored",
+        separator_style = "default",
+        -- order = {
+        --     "mode",
+        --     "git",
+        --     "%=",
+        --     "lsp_msg",
+        --     "%=",
+        --     "lsp",
+        --     "cwd",
+        --     "cursor",
+        -- "location",
+        -- },
+        -- modules = {
+        --     lsp = function()
+        --         if rawget(vim, "lsp") then
+        --             for _, client in ipairs(vim.lsp.get_clients()) do
+        --                 if client.name == "null-ls" then
+        --                     goto continue
+        --                 end
+        --                 if
+        --                     client.attached_buffers[vim.api.nvim_win_get_buf(
+        --                         vim.g.statusline_winid or 0
+        --                     )]
+        --                 then
+        --                     return (
+        --                         vim.o.columns > 100
+        --                         and "   LSP ~ " .. client.name .. " "
+        --                     ) or "   LSP "
+        --                 end
+        --                 ::continue::
+        --             end
+        --         end
+        --
+        --         return ""
+        --     end,
+        --     location = "%#StText# ln: %l, col: %c ",
+        -- },
     },
 }
 
