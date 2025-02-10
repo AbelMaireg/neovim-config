a971b06f512e351e1cb6a861b4d3761bf7ec2e88
AbelMaireg <abelrighthere@gmail.com>
2025-02-10 14:18:27 +0300

    feat: snacks.scop enabled with z prefiz mappings.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,17 +10,45 @@ return {
         ---@type snacks.Config
         opts = {
             bigfile = { enabled = false },
-            dashboard = { enabled = true },
+            dashboard = {
+                enabled = true,
+                preset = {
+                    header = [[AbelMaireg]],
+                },
+            },
             explorer = { enabled = false },
+            gitbrowse = { enabled = true },
             indent = { enabled = true },
             input = { enabled = false },
             picker = { enabled = false },
             notifier = { enabled = true },
             quickfile = { enabled = false },
-            scope = { enabled = false },
+            scope = {
+                enabled = true,
+                keys = {
+                    jump = {
+                        ["zk"] = {
+                            min_size = 1,
+                            bottom = false,
+                            cursor = false,
+                            edge = true,
+                            treesitter = { blocks = { enabled = true } },
+                            desc = "jump to top edge of scope",
+                        },
+                        ["zj"] = {
+                            min_size = 1,
+                            bottom = true,
+                            cursor = false,
+                            edge = true,
+                            treesitter = { blocks = { enabled = true } },
+                            desc = "jump to bottom edge of scope",
+                        },
+                    },
+                },
+            },
             scroll = { enabled = true },
-            statuscolumn = { enabled = false },
-            words = { enabled = false },
+            statuscolumn = { enabled = true },
+            words = { enabled = true },
         },
     },
 }
