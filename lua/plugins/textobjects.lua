10e79366f482dae98d71e966ea3be4325ad9de78
AbelMaireg <abelrighthere@gmail.com>
2025-02-15 11:38:16 +0300

    feat: treesitter-textobjects enabled successfully.

---
+++ lua/plugins/textobjects.lua
@@ -0,0 +1,97 @@
+return {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    lazy = true,
+    event = "VeryLazy",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+
+                        ["al"] = "@loop.outer",
+                        ["il"] = "@loop.inner",
+
+                        ["ab"] = "@block.outer",
+                        ["ib"] = "@block.inner",
+
+                        ["as"] = "@statement.outer",
+
+                        -- ["as"] = {
+                        --     query = "@local.scope",
+                        --     query_group = "locals",
+                        --     desc = "Select language scope",
+                        -- },
+                    },
+                    selection_modes = {
+                        ["@parameter.outer"] = "v",
+                        ["@function.outer"] = "V",
+                        ["@class.outer"] = "<c-v>",
+                    },
+                    include_surrounding_whitespace = true,
+                },
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["<leader>sn"] = "@parameter.inner",
+                    },
+                    swap_previous = {
+                        ["<leader>sp"] = "@parameter.inner",
+                    },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        ["]f"] = "@function.outer",
+                        ["]c"] = "@class.outer",
+                        ["]l"] = "@loop.*",
+                        ["]s"] = {
+                            query = "@local.scope",
+                            query_group = "locals",
+                            desc = "Next scope",
+                        },
+                        ["]z"] = {
+                            query = "@fold",
+                            query_group = "folds",
+                            desc = "Next fold",
+                        },
+                    },
+                    goto_next_end = {
+                        ["]F"] = "@function.outer",
+                        ["]C"] = "@class.outer",
+                    },
+                    goto_previous_start = {
+                        ["[f"] = "@function.outer",
+                        ["[c"] = "@class.outer",
+                    },
+                    goto_previous_end = {
+                        ["[F"] = "@function.outer",
+                        ["[C"] = "@class.outer",
+                    },
+                    goto_next = {
+                        ["]d"] = "@conditional.outer",
+                    },
+                    goto_previous = {
+                        ["[d"] = "@conditional.outer",
+                    },
+                },
+                lsp_interop = {
+                    enable = true,
+                    border = "none",
+                    floating_preview_opts = {},
+                    peek_definition_code = {},
+                },
+            },
+        })
+    end,
+}
--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,6 +1,10 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        dependecies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
         opts = {
             highlight = {
                 enable = true,
@@ -58,5 +62,35 @@ return {
                 "yaml",
             },
         },
+        configs = function()
+            require("nvim-treesitter.configs").setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = {
+                                query = "@class.inner",
+                                desc = "Select inner part of a class region",
+                            },
+                            ["as"] = {
+                                query = "@local.scope",
+                                query_group = "locals",
+                                desc = "Select language scope",
+                            },
+                        },
+                        selection_modes = {
+                            ["@parameter.outer"] = "v", -- charwise
+                            ["@function.outer"] = "V",  -- linewise
+                            ["@class.outer"] = "<c-v>", -- blockwise
+                        },
+                        include_surrounding_whitespace = true,
+                    },
+                },
+            })
+        end,
     },
 }
