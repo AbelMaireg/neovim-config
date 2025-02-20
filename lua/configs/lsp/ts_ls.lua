20bc195583ef5d18f45fe7747ca89fb664afedff
AbelMaireg <abelrighthere@gmail.com>
2025-02-20 20:15:52 +0300

    feat: inlay hint for ts, js and vue enabled.

--- lua/configs/lsp/ts_ls.lua
+++ lua/configs/lsp/ts_ls.lua
@@ -1,3 +1,24 @@
+function inlayHintsConfigGenPerLang (
+    enum,
+    func_like_return,
+    func_param,
+    param_name,
+    param_name_arg_match,
+    prop_decl,
+    variable_type
+)
+    return {
+        includeInlayEnumMemberValueHints = enum or true,
+        includeInlayFunctionLikeReturnTypeHints = func_like_return or true,
+        includeInlayFunctionParameterTypeHints = func_param or true,
+        includeInlayParameterNameHints = param_name or "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = param_name_arg_match
+            or true,
+        includeInlayPropertyDeclarationTypeHints = prop_decl or true,
+        includeInlayVariableTypeHints = variable_type or true,
+    }
+end
+
 return {
     on_attach = on_attach,
     on_init = on_init,
@@ -22,5 +43,19 @@ return {
         "javascriptreact",
         "typescriptreact",
         "vue",
+    },
+    settings = {
+        javascript = {
+            inlayHints = inlayHintsConfigGenPerLang (),
+        },
+        typescript = {
+            inlayHints = inlayHintsConfigGenPerLang (),
+        },
+        vue = {
+            inlayHints = inlayHintsConfigGenPerLang (),
+        },
+    },
+    inlay_hints = {
+        enabled = true,
     },
 }
