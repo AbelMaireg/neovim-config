439a3a370a97fa959ad7ac90d064b772577d7b16
AbelMaireg <abelrighthere@gmail.com>
2025-02-21 16:28:16 +0300

    fix: null_ls exculusion enabled successfully.

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,6 +7,42 @@ M.ui = {
     statusline = {
         theme = "minimal",
         separator_style = "default",
+        order = {
+            "mode",
+            "git",
+            "%=",
+            "lsp_msg",
+            "%=",
+            "diagnostics",
+            "lsp",
+            "cwd",
+            "cursor",
+        },
+        modules = {
+            lsp = function ()
+                if rawget (vim, "lsp") then
+                    for _, client in ipairs (vim.lsp.get_clients ()) do
+                        if client.name == "null-ls" then
+                            goto continue
+                        end
+                        if
+                            client.attached_buffers[vim.api.nvim_win_get_buf (
+                                vim.g.statusline_winid or 0
+                            )]
+                        then
+                            return (
+                                vim.o.columns > 100
+                                and "   LSP ~ " .. client.name .. " "
+                            ) or "   LSP "
+                        end
+                        ::continue::
+                    end
+                end
+
+                return ""
+            end,
+            cursor = "%#StText#%#St_file_txt# l:%3l, c:%3c ",
+        },
     },
 }
 
