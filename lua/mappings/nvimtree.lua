bb30c3f2933e0ffe8a6a9de0f1bab3f9e23bd655
AbelMaireg <abelrighthere@gmail.com>
2025-02-23 18:54:00 +0300

    feat: equalize window size of tree toggle

--- lua/mappings/nvimtree.lua
+++ lua/mappings/nvimtree.lua
@@ -3,7 +3,7 @@ local map = vim.keymap.set
 map (
     "n",
     "<C-n>",
-    "<cmd>NvimTreeToggle<CR><C-w>=",
+    "<cmd>NvimTreeToggle<CR><cmd>wincmd=<CR>",
     { desc = "nvimtree toggle window" }
 )
 map (
