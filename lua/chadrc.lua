ce4ca1f7cc993c8935e214b24b30f6f8c2ba21f5
AbelMaireg <abelrighthere@gmail.com>
2025-02-23 19:39:24 +0300

    feat: number of lines added to statusline.

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,5 +1,5 @@
-require ("configs.nvterm")
-require ("utils.contains")
+require("configs.nvterm")
+require("utils.contains")
 
 ---@type ChadrcConfig
 local M = {}
@@ -21,18 +21,19 @@ M.ui = {
             "null_ls",
             "cwd",
             "cursor",
+            "lines",
         },
         modules = {
-            lsp = function ()
+            lsp = function()
                 local exo = { "null-ls", "GitHub Copilot" }
 
-                if rawget (vim, "lsp") then
-                    for _, client in ipairs (vim.lsp.get_clients ()) do
-                        if contains (exo, client.name) then
+                if rawget(vim, "lsp") then
+                    for _, client in ipairs(vim.lsp.get_clients()) do
+                        if contains(exo, client.name) then
                             goto continue
                         end
                         if
-                            client.attached_buffers[vim.api.nvim_win_get_buf (0)]
+                            client.attached_buffers[vim.api.nvim_win_get_buf(0)]
                         then
                             return (
                                 vim.o.columns > 100
@@ -45,25 +46,30 @@ M.ui = {
 
                 return ""
             end,
-            null_ls = function ()
-                for _, client in ipairs (vim.lsp.get_clients ()) do
+            null_ls = function()
+                for _, client in ipairs(vim.lsp.get_clients()) do
                     if client.name == "null-ls" then
-                        return " ✨ "
+                        return "✨ "
                     end
                 end
 
                 return ""
             end,
-            copilot = function ()
-                for _, client in ipairs (vim.lsp.get_clients ()) do
+            copilot = function()
+                for _, client in ipairs(vim.lsp.get_clients()) do
                     if client.name == "GitHub Copilot" then
-                        return " 👾 "
+                        return "👾 "
                     end
                 end
 
                 return ""
             end,
             cursor = "%#St_file_txt#%4l :%3c ",
+            lines = function()
+                return "%#St_File_bg# "
+                    .. vim.api.nvim_buf_line_count(0)
+                    .. " "
+            end,
         },
     },
 }
