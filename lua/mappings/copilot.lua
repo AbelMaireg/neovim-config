d87e10ebb243f4c31ff440fc555d18914fc4fb0b
AbelMaireg <abelrighthere@gmail.com>
2025-02-24 15:01:20 +0300

    fix: copilot keymaps set to starting letter of command.

--- lua/mappings/copilot.lua
+++ lua/mappings/copilot.lua
@@ -1,36 +1,42 @@
 local map = vim.keymap.set
 
-map ("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+-- A accept
+map ("i", "<C-A>", 'copilot#Accept("\\<CR>")', {
     expr = true,
     replace_keycodes = false,
     desc = "Accept the current completion item",
 })
 
-map ("i", "<C-R>", "<Plug>(copilot-dismiss)", {
+-- D dismiss
+map ("i", "<C-D>", "<Plug>(copilot-dismiss)", {
     expr = false,
     replace_keycodes = false,
     desc = "Dismiss the current completion item",
 })
 
-map ("i", "<C-K>", "<Plug>(copilot-previous)", {
+-- P goto previous
+map ("i", "<C-P>", "<Plug>(copilot-previous)", {
     expr = false,
     replace_keycodes = false,
     desc = "Go to the previous completion item",
 })
 
-map ("i", "<C-J>", "<Plug>(copilot-next)", {
+-- N goto next
+map ("i", "<C-N>", "<Plug>(copilot-next)", {
     expr = false,
     replace_keycodes = false,
     desc = "Go to the next completion item",
 })
 
-map ("i", "<C-L>", "<Plug>(copilot-accept-word)", {
+-- W accept word
+map ("i", "<C-W>", "<Plug>(copilot-accept-word)", {
     expr = false,
     replace_keycodes = false,
     desc = "Accept the current completion item and move to the next word",
 })
 
-map ("i", "<C-O>", "<Plug>(copilot-accept-line)", {
+-- L accept line
+map ("i", "<C-L>", "<Plug>(copilot-accept-line)", {
     expr = false,
     replace_keycodes = false,
     desc = "Accept the current completion item and move to the next line",
