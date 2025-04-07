cbcd1d2c48b646770e5119a880b82832e4df24eb
AbelMaireg <abelrighthere@gmail.com>
2025-04-08 02:33:21 +0300

    refactor: conform setup moved to configs

---
+++ lua/configs/conform.lua
@@ -0,0 +1,59 @@
+return {
+    formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = {
+            "eslint_d",
+            "prettierd",
+            "prettier",
+            "oxlint",
+            stop_after_first = true,
+        },
+        typescript = {
+            "eslint_d",
+            "prettierd",
+            "prettier",
+            "oxlint",
+            stop_after_first = true,
+        },
+        vue = {
+            "eslint_d",
+            "prettierd",
+            "prettier",
+            "oxlint",
+            stop_after_first = true,
+        },
+        java = {
+            "google-java-format",
+        },
+        kotlin = {
+            "ktfmt",
+            -- "ktlint",
+        },
+        bash = {
+            "shfmt",
+        },
+        fish = {
+            "shfmt",
+        },
+        yaml = {
+            "yamlfmt",
+            "yamllint",
+        },
+        zig = {
+            "zls",
+        },
+        docker = {
+            "dockerls",
+        },
+        sql = {
+            "sql_formatter",
+        },
+        dart = {
+            "dart_format",
+        },
+    },
+    format_on_save = {
+        timeout_ms = 1000,
+        lsp_format = "fallback",
+    },
+}
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,65 +2,7 @@ return {
     {
         "stevearc/conform.nvim",
         config = function ()
-            require ("conform").setup ({
-                formatters_by_ft = {
-                    lua = { "stylua" },
-                    javascript = {
-                        "eslint_d",
-                        "prettierd",
-                        "prettier",
-                        "oxlint",
-                        stop_after_first = true,
-                    },
-                    typescript = {
-                        "eslint_d",
-                        "prettierd",
-                        "prettier",
-                        "oxlint",
-                        stop_after_first = true,
-                    },
-                    vue = {
-                        "eslint_d",
-                        "prettierd",
-                        "prettier",
-                        "oxlint",
-                        stop_after_first = true,
-                    },
-                    java = {
-                        "google-java-format",
-                    },
-                    kotlin = {
-                        "ktfmt",
-                        -- "ktlint",
-                    },
-                    bash = {
-                        "shfmt",
-                    },
-                    fish = {
-                        "shfmt",
-                    },
-                    yaml = {
-                        "yamlfmt",
-                        "yamllint",
-                    },
-                    zig = {
-                        "zls",
-                    },
-                    docker = {
-                        "dockerls",
-                    },
-                    sql = {
-                        "sql_formatter",
-                    },
-                    dart = {
-                        "dart_format",
-                    },
-                },
-                format_on_save = {
-                    timeout_ms = 1000,
-                    lsp_format = "fallback",
-                },
-            })
+            require ("conform").setup (require ("configs.conform"))
         end,
     },
 }
