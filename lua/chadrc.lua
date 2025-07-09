3545ede5c364ae9fc881b61a464d10f31071a5c1
AbelMaireg <abelrighthere@gmail.com>
2025-07-09 12:10:08 +0300

    build: plugin updates.

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -79,9 +79,9 @@ M.ui = {
 }
 
 M.base46 = {
-    theme = "ayu_dark",
+    theme = "catppuccin",
     transparency = false,
-    theme_toggle = { "tokyonight", "ayu_dark" },
+    theme_toggle = { "tokyonight", "catppuccin" },
 
     hl_override = {
         ["@comment"] = { fg = "#afafaf", italic = true },
