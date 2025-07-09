0b34e6788afd6455eff37b599f7e34f1bb182bc7
AbelMaireg <abelrighthere@gmail.com>
2025-07-09 11:56:49 +0300

    feat: enhancement on nvim-tree config.

--- lua/configs/lsp/phpactor.lua
+++ lua/configs/lsp/phpactor.lua
@@ -3,7 +3,10 @@ return {
         return vim.loop.cwd ()
     end,
     init_options = {
-        ["language_server_phpstan.enabled"] = true,
-        ["language_server_psalm.enabled"] = true,
+        ["language_server.diagnostics_on_update"] = true,
+        ["language_server.diagnostics_on_open"] = true,
+        ["language_server.diagnostics_on_save"] = true,
+        ["language_server_phpstan.enabled"] = false,
+        ["language_server_psalm.enabled"] = false,
     },
 }
