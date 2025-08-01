4130d884ef8e78d367fee995dd9c75b64893b194
AbelMaireg <abelrighthere@gmail.com>
2025-08-01 21:42:29 +0300

    style: lint

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -79,9 +79,9 @@ M.ui = {
 }
 
 M.base46 = {
-    theme = "catppuccin",
+    theme = "ayu_dark",
     transparency = false,
-    theme_toggle = { "tokyonight", "catppuccin" },
+    theme_toggle = { "tokyonight", "ayu_dark" },
 
     hl_override = {
         ["@comment"] = { fg = "#afafaf", italic = true },
--- lua/mappings/nvterm.lua
+++ lua/mappings/nvterm.lua
@@ -9,28 +9,3 @@ for _, mapping in ipairs ({
         require ("nvterm.terminal").toggle (mapping[2])
     end, { desc = "terminal" .. mapping[2] })
 end
-
-map (
-    "t",
-    "<C-j>",
-    "<C-\\><C-N><C-j>",
-    { desc = "terminal escape terminal mode" }
-)
-map (
-    "t",
-    "<C-k>",
-    "<C-\\><C-N><C-k>",
-    { desc = "terminal escape terminal mode" }
-)
-map (
-    "t",
-    "<C-h>",
-    "<C-\\><C-N><C-h>",
-    { desc = "terminal escape terminal mode" }
-)
-map (
-    "t",
-    "<C-l>",
-    "<C-\\><C-N><C-l>",
-    { desc = "terminal escape terminal mode" }
-)
