b837dacac9975637d2c7418ffe8c53e77c3f83f0
AbelMaireg <abelrighthere@gmail.com>
2025-09-21 15:50:38 +0300

    fix: avantes custom copilot plugin disabled

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,25 +1,16 @@
 return {
     {
         "mrcjkb/rustaceanvim",
-        version = "^5",
+        version = "^6",
         lazy = true,
         ft = { "rust" },
         config = function ()
-            local mason_registry = require ("mason-registry")
-            local codelldb = mason_registry.get_package ("codelldb")
-            local extension_path = codelldb:get_install_path () .. "/extension/"
-            local codelldb_path = extension_path .. "adapter/codelldb"
-            local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
-            local cfg = require ("rustaceanvim.config")
-
-            vim.g.rustaceanvim = {
-                dap = {
-                    adapter = cfg.get_codelldb_adapter (
-                        codelldb_path,
-                        liblldb_path
-                    ),
-                },
-            }
+            -- local mason_registry = require ("mason-registry")
+            -- local codelldb = mason_registry.get_package ("codelldb")
+            -- local extension_path = codelldb:get_install_path () .. "/extension/"
+            -- local codelldb_path = extension_path .. "adapter/codelldb"
+            -- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+            -- local cfg = require ("rustaceanvim.config")
 
             require ("mappings.vim-lsp")
             require ("mappings.vim-diagonistics")
