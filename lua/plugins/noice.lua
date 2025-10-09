d805d0c66c175e6f1115ae4dff4fa5906e76de3b
AbelMaireg <abelrighthere@gmail.com>
2025-10-09 14:19:00 +0300

    fix: noice reinstalled

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,15 +1,11 @@
 return {
     "folke/noice.nvim",
     event = "VeryLazy",
-    enabled = true,
-    opts = require ("configs.noice"),
-    config = function (_, opts)
-        -- HACK: noice shows messages from before it was enabled,
-        -- but this is not ideal when Lazy is installing plugins,
-        -- so clear the messages in this case.
-        if vim.o.filetype == "lazy" then
-            vim.cmd ([[messages clear]])
-        end
-        require ("noice").setup (opts)
-    end,
+    opts = {
+        -- add any options here
+    },
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
 }
