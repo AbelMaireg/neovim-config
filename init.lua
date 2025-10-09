9d79793e0a9c3f888f468ebe99b9712b723d77fb
AbelMaireg <abelrighthere@gmail.com>
2025-10-09 08:43:00 +0300

    build: NvChad based plugin totally removed.

--- init.lua
+++ init.lua
@@ -20,19 +20,12 @@ vim.opt.rtp:prepend (lazypath)
 local lazy_config = require ("configs.lazy")
 
 require ("lazy").setup ({
-    {
-        "NvChad/NvChad",
-        lazy = false,
-        branch = "v2.5",
-        import = "nvchad.plugins",
-    },
-
     { import = "plugins" },
 }, lazy_config)
 
--- load theme
-dofile (vim.g.base46_cache .. "defaults")
-dofile (vim.g.base46_cache .. "statusline")
+for _, v in ipairs (vim.fn.readdir (vim.g.base46_cache)) do
+    dofile (vim.g.base46_cache .. v)
+end
 
 require ("options")
 
--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -79,9 +79,9 @@ M.ui = {
 }
 
 M.base46 = {
-    theme = "radium",
+    theme = "rosepine",
     transparency = false,
-    theme_toggle = { "tokyonight", "radium" },
+    theme_toggle = { "tokyonight", "rosepine" },
 
     hl_override = {
         ["@comment"] = { fg = "#afafaf", italic = true },
