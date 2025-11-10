a731912a5c1e6f73656c136357ef44a2eb9062dc
AbelMaireg <abelrighthere@gmail.com>
2025-11-10 11:19:33 +0300

    feat: vue volar2.0 + typescript dual-LSP

--- lua/configs/lsp/ts_ls.lua
+++ lua/configs/lsp/ts_ls.lua
@@ -1,37 +1,20 @@
-inlay_hint_conf_gen = require ("utils.inlay-hint-conf-gen")
-
 return {
-    single_file_support = false,
-    root_dir = vim.fs.root (0, ".git"),
+    root_dir = vim.fs.root("0", ".git"),
     init_options = {
         plugins = {
             {
                 name = "@vue/typescript-plugin",
-                location = vim.fn.stdpath ("data")
-                    .. "/mason/packages/vue-language-server/node_modules"
-                    .. "/@vue/language-server/node_modules"
-                    .. "/@vue/typescript-plugin",
+                location = vim.fn.stdpath("data")
+                    .. "/mason/packages/"
+                    .. "/vue-language-server/node_modules/"
+                    .. "/@vue/language-server",
                 languages = { "vue" },
             },
         },
-        hybridMode = false,
     },
     filetypes = {
         "typescript",
         "javascript",
-        "javascriptreact",
-        "typescriptreact",
         "vue",
-    },
-    settings = {
-        -- javascript = {
-        --     inlayHints = inlay_hint_conf_gen (),
-        -- },
-        -- typescript = {
-        --     inlayHints = inlay_hint_conf_gen (),
-        -- },
-    },
-    inlay_hints = {
-        enabled = true,
     },
 }
