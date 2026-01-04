dae7ebf14604cce3fcd4039a6b2dadc00af3c4b9
AbelMaireg <abelrighthere@gmail.com>
2026-01-04 18:59:37 +0300

    feat: set git_blame using git signs

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -10,6 +10,14 @@ return {
                     delete = { text = "󰍵" },
                     changedelete = { text = "󱕖" },
                 },
+                current_line_blame = true,
+                current_line_blame_opts = {
+                    virt_text = true,
+                    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+                    delay = 1000, -- Delay in milliseconds before blame shows
+                    ignore_whitespace = false,
+                    virt_text_priority = 100,
+                },
             }
         end,
     },
