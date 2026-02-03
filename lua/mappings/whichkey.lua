02e8acf971bb0d18a447974ed913dbd0381e9b0a
AbelMaireg <abelrighthere@gmail.com>
2026-02-03 12:30:59 +0300

    feat: mapping for clipping the file path and cursor position

--- lua/mappings/whichkey.lua
+++ lua/mappings/whichkey.lua
@@ -5,3 +5,14 @@ map ("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
 map ("n", "<leader>wk", function ()
     vim.cmd ("WhichKey " .. vim.fn.input ("WhichKey: "))
 end, { desc = "whichkey query lookup" })
+
+map ("n", "<leader>yp", function ()
+    local location = string.format (
+        "%s:%d:%d",
+        vim.fn.expand ("%"),
+        vim.fn.line ("."),
+        vim.fn.col (".")
+    )
+
+    vim.fn.setreg ("+", location)
+end, { desc = "Copy relative file:line:col to system clipboard" })
